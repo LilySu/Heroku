d10421bf2efd6643503ae69fd59c6e507e1aa0b9
Lily Su <lilyxsu@gmail.com>
2019-07-01 12:46:55 -0400

    first

---
+++ tabs/evaluate.py
@@ -0,0 +1,34 @@
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from joblib import load
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from plotly.tools import mpl_to_plotly
+import seaborn as sns
+
+from app import app
+
+pipeline = load('model/pipeline.joblib')
+history = pd.read_csv('model/lending-club.csv')
+X = history.drop(columns='Interest Rate')
+y_pred_log = pipeline.predict(X)
+y_pred = np.expm1(y_pred_log)
+
+fig, ax = plt.subplots()
+sns.distplot(history['Interest Rate'], hist=False, kde=True, ax=ax, label='Actual')
+sns.distplot(y_pred, hist=False, kde=True, ax=ax, label='Predicted')
+ax.set_title('Distribution of predictions is simpler and less spread than actuals')
+ax.legend().set_visible(False)
+plotly_fig = mpl_to_plotly(fig)
+plotly_fig['layout']['showlegend'] = True
+
+layout = [dcc.Markdown("""
+### Evaluate
+
+"""), 
+
+dcc.Graph(id='evaluate-graph', figure=plotly_fig)
+]
